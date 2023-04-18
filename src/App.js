f8a9050648149588447e457e8d2339d34b87b82f
StevenMo <stevenmolvin@gmail.com>
2023-04-18 09:46:55 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const handleClick = () => {
+    alert('You clicked this button');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src="https://i.pinimg.com/236x/f6/4d/45/f64d4591c7c1088c5ff87ec41bfb0acf.jpg"
+        alt="How to tie ties"
+      />
+      <p>How to tie a tie. </p>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
+  
 }
 
 export default App;
